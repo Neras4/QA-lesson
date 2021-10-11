c4937eee3540d78119b65a2f085ce41b02c01d00
Neras4 <solovbeloz@gmail.com>
2021-10-11 14:21:23 +0300

    last changes before end

--- test_product_page.py
+++ test_product_page.py
@@ -42,7 +42,7 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     page.go_to_login_page()
 
 
-@pytest.mark.inheritance
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
@@ -51,7 +51,7 @@ def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     page = BasketPage(browser, browser.current_url)
     page.should_be_basket_empty_message()
 
-@pytest.mark.inheritance
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
@@ -60,7 +60,7 @@ def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     page = BasketPage(browser, browser.current_url)
     page.is_basket_empty()
 
-@pytest.mark.nnheritance
+@pytest.mark.need_review
 def test_guest_can_add_product_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     product_page = ProductPage(browser, link)
